332207ec6c43465d3f3ea429e47e1e2f1891fc6f
prodaptconsulting <42060574+prodaptconsulting@users.noreply.github.com>
2018-08-20 17:23:45 +0200

    Update AddForwardRule.sh

--- AddForwardRule.sh
+++ AddForwardRule.sh
@@ -3,6 +3,6 @@ curl_data="--data '{\"id\": 1,\"method\":\"login\",\"params\":[\"root\",\"\"]}'"
 curl_cmd="curl -s "
 curl_final=$curl_cmd$curl_url$curl_data
 echo $curl_final
-Auth=$(curl_final)
+Auth=$curl_final
 Result=`echo $Auth | jq .result`
 echo $Result
