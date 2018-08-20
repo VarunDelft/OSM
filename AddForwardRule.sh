558e7b5e1f1cffcab1329cd1d73b1dac00acc0f8
prodaptconsulting <42060574+prodaptconsulting@users.noreply.github.com>
2018-08-20 17:49:06 +0200

    Update AddForwardRule.sh

--- AddForwardRule.sh
+++ AddForwardRule.sh
@@ -2,7 +2,7 @@ curl_url="http://"$1"/cgi-bin/luci/rpc/auth "
 curl_data="--data '{\"id\": 1,\"method\":\"login\",\"params\":[\"root\",\"\"]}'"
 curl_cmd="curl -s "
 curl_final=$curl_cmd$curl_url$curl_data
-echo $curl_final
+echo $curl_url$curl_data
 curl -s $curl_url$curl_data
 echo $Auth
 Result=echo $Auth | jq .result
