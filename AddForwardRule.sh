c372d903937982974e2a2a8030610221aa007180
prodaptconsulting <42060574+prodaptconsulting@users.noreply.github.com>
2018-08-20 17:21:44 +0200

    Update AddForwardRule.sh

--- AddForwardRule.sh
+++ AddForwardRule.sh
@@ -1,5 +1,5 @@
 curl_url="http://"$1"/cgi-bin/luci/rpc/auth "
-curl_data = '--data \'{"id": 1,"method":"login","params":["root",""]}\''
+curl_data="--data \'{\"id\": 1,\"method\":\"login\",\"params\":[\"root\",\"\"]}\'"
 curl_cmd="curl -s "
 curl_final=$curl_cmd$curl_url$curl_data
 echo $curl_final
