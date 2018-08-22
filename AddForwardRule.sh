90d7ebebba2fa63423c024fc2bc09d74d373f764
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-22 12:52:40 +0000

    Changed file structure

--- AddForwardRule.sh
+++
@@ -1,9 +0,0 @@
-curl_url="http://"$1"/cgi-bin/luci/rpc/auth "
-curl_data="--data '{\"id\": 1,\"method\":\"login\",\"params\":[\"root\",\"\"]}'"
-curl_cmd="curl -s "
-curl_final=$curl_cmd$curl_url$curl_data
-echo ${curl_cmd}  ${curl_url} ${curl_data}
-Auth=`${curl_cmd}  ${curl_url} ${curl_data}`
-echo ${Auth}
-Result=echo ${Auth} | jq .result
-echo $Result
