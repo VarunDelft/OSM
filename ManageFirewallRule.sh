a5210a05694a899d2ae2c77105d984d3ab97a9e4
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-23 13:20:50 +0200

    a

--- ManageFirewallRule.sh
+++ ManageFirewallRule.sh
@@ -38,6 +38,23 @@ else
 fi
 }
 
+DeleteFirewallRule(){
+HTTPS_URL="http://"$1"/cgi-bin/luci/rpc/uci?auth="$Result" -d @- "
+HTTPS_DATA=`cat DeleteRuleData.json`
+CURL_CMD_FINAL=${CURL_CMD}${CURL_MAX_CONNECTION_TIMEOUT}${HTTPS_URL}
+CURL_RETURN_CODE=0
+CURL_OUTPUT=`echo ${HTTPS_DATA} | ${CURL_CMD_FINAL} 2> /dev/null` || CURL_RETURN_CODE=$?
+
+if [ ${CURL_RETURN_CODE} -ne 0 ]
+then
+    RETURN_CODE=1
+    echo "error in Deleting firewall rule"
+    echo ${CURL_OUTPUT}
+else
+    RETURN_CODE=0
+fi
+}
+
 CommitFirewallRule(){
 HTTPS_URL="http://"$1"/cgi-bin/luci/rpc/uci?auth="$Result" -d @- "
 HTTPS_DATA=`cat CommitRuleData.json`
@@ -88,6 +105,15 @@ fi
 if [ $3 = "Open" ]
 then
     AddFirewallRule $1 $2
+    if [ ${RETURN_CODE} -ne 0 ]
+    then
+       exit ${RETURN_CODE}
+    fi
+fi
+
+if [ $3 = "Block" ]
+then
+    DeleteFirewallRule $1
     if [ ${RETURN_CODE} -ne 0 ]
     then
        exit ${RETURN_CODE}
