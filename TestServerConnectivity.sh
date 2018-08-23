ec0e5e4a985fc5042af672b09f89aaed1dd88585
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-23 14:03:57 +0200

    a

--- TestServerConnectivity.sh
+++ TestServerConnectivity.sh
@@ -8,7 +8,9 @@ CURL_RETURN_CODE=0
 CURL_OUTPUT=`${CURL_CMD} ${CURL_MAX_CONNECTION_TIMEOUT} ${HTTPS_URL} 2> /dev/null` || CURL_RETURN_CODE=$?
 if [ ${CURL_RETURN_CODE} -ne 0 ]
 then
-    RETURN_CODE=1
+    echo "Curl unsuccessfull"
+	echo ${CURL_RETURN_CODE}
+	RETURN_CODE=1
 else
     #echo "Curl connection success"
     # Check http code for curl operation/response in  CURL_OUTPUT
