97b043371d0a52e3007f79fd1db118ae9d91cbd5
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-29 14:49:32 +0000

    adf

--- Scripts/OSMNSInstantiate.sh
+++ Scripts/OSMNSInstantiate.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 GetAuthcode(){
 D=`cat Data/OSMAuthRequest.json`
-D=${D//__USER_NAME__/$2}
-D=${D//__PASSWORD__/$3}
+#D=${D//__USER_NAME__/$2}
+#D=${D//__PASSWORD__/$3}
+D=`echo $D | sed -e "s/__USER_NAME__/${2}/g"`
+D=`echo $D | sed -e "s/__PASSWORD__/${3}/g"`
 RETURN_CODE=0
 CURL_RETURN_CODE=0
 CURL_OUTPUT=`echo $D | curl -k -d @- -m 100 -H "Accept:application/json" -H "Content-Type:application/json" -X POST https://$1/osm/admin/v1/tokens 2> /dev/null`  || CURL_RETURN_CODE=$?
