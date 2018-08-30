c1813d5538c3e94e13ca972a2edffe564fc5cb1c
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-30 16:25:40 +0200

    1

---
+++ Scripts/OSMNSGetDetails.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+GetNSDetails(){
+
+RETURN_CODE=0
+CURL_RETURN_CODE=0
+#Abc="curl -k -d @- -m 100 -H \"Accept:application/json\" -H \"Authorization: Bearer $1\" -H \"Content-Type:application/json\" -X POST https://$2/osm/nslcm/v1/ns_instances/$3"
+CURL_OUTPUT=`curl -k  -m 100 -H "Accept:application/json" -H "Authorization: Bearer $1" -H "Content-Type:application/json" -X POST https://$2/osm/nslcm/v1/ns_instances/$3 2> /dev/null`  || CURL_RETURN_CODE=$?
+if [ ${CURL_RETURN_CODE} -ne 0 ]
+then
+    RETURN_CODE=1
+    echo "error in getting outhorization code for ${CURL_OUTPUT}"
+else
+    RETURN_CODE=0
+    Result=${CURL_OUTPUT}
+fi
+return $RETURN_CODE
+}
+
+
+#Main Script Start Here
+# $1 = Authorisation code
+# $2 = OSM IP address:Port
+# $3 = Instance id of the network service
+
+
+NSDetails=$(GetNSDetails "${1}" "${2}" "$3")
+RETURN_CODE=`echo $?`
+echo ${NSDetails}
+exit $RETURN_CODE
