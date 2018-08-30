0613de7f8d74140bf54c185edb593d79dc60fca0
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-08-30 12:40:49 +0000

    asdfdfDSF'ds

--- Scripts/OSMNSInstantiate.sh
+++ Scripts/OSMNSInstantiate.sh
@@ -8,12 +8,12 @@ D="{
 }"
 RETURN_CODE=0
 CURL_RETURN_CODE=0
-Abc='curl -k -d @- -m 100 -H "Accept:application/json" -H "Authorization: Bearer $1" -H "Content-Type:application/json" -X POST https://$2/osm/admin/v1/tokens'
+Abc='curl -k -d @- -m 100 -H "Accept:application/json" -H "Authorization: Bearer ${1}" -H "Content-Type:application/json" -X POST https://{$2}/osm/admin/v1/tokens'
 CURL_OUTPUT=`echo $D | curl -k -d @- -m 100 -H "Accept:application/json" -H "Authorization: Bearer $1" -H "Content-Type:application/json" -X POST https://$2/osm/nslcm/v1/ns_instances/$7/instantiate 2> /dev/null`  || CURL_RETURN_CODE=$?
 if [ ${CURL_RETURN_CODE} -ne 0 ]
 then
     RETURN_CODE=1
-	echo $D
+    echo $D
     echo "error in getting outhorization code for ${CURL_OUTPUT}"
 else
     RETURN_CODE=0
@@ -23,6 +23,7 @@ else
     then
        RETURN_CODE=1
        echo "${CURL_OUTPUT}"
+       echo "${Abc}"
        echo "Error in Creating Network Service"
     else
        Result=`echo ${CURL_OUTPUT} | jq .id`
