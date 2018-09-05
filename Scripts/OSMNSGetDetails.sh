a7a7b7bf34388d629ba283d0dbc7488f3a366afe
prodaptconsulting <jignesh.karnik@prodapt.com>
2018-09-05 16:00:49 +0200

    a

--- Scripts/OSMNSGetDetails.sh
+++ Scripts/OSMNSGetDetails.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GetStatus(){
+GetNSDetails(){
 RETURN_CODE=0
 CURL_RETURN_CODE=0
         Abc="curl -k  -m 100 -H \"Accept:application/json\" -H \"Authorization: Bearer $1\" -H \"Content-Type:application/json\" -X GET https://$2/osm/nslcm/v1/ns_instances/$3"
@@ -18,7 +18,30 @@ then
 else
     #echo $Abc
     RETURN_CODE=0
-    echo "Ready"
+    #echo "${CURL_OUTPUT}"
+    const_vnfds=`echo ${CURL_OUTPUT} | jq .\"nsd\".\"constituent-vnfd\"`
+    #echo "$const_vnfds"
+    fw_index=`echo ${const_vnfds} | jq '.[] | select(."vnfd-id-ref" | contains("fw")) | ."member-vnf-index"' | sed "s/\"//g"`
+    server_index=`echo ${const_vnfds} | jq '.[] | select(."vnfd-id-ref" | contains("server")) | ."member-vnf-index"'  | sed "s/\"//g"`
+    #echo "Firewall index is $fw_index"
+    #echo "Server index is $server_index"
+    const_vnfrs=`echo ${CURL_OUTPUT} | jq .\"constituent-vnfr-ref\"`
+    #echo $const_vnfrs
+    fw_id=`echo ${CURL_OUTPUT} | jq .\"constituent-vnfr-ref\"[$(($fw_index-1))]`
+    server_id=`echo ${CURL_OUTPUT} | jq .\"constituent-vnfr-ref\"[$(($server_index-1))]`
+    #echo "Firewall id is $fw_id"
+    #echo "Server id is $server_id"
+    vlds=`echo ${CURL_OUTPUT} | jq .\"nsd\".\"vld\"`
+    #mgmt_index=`echo ${vlds} | jq 'map(select(."short-name" | contains("mgmt"))) | index(true)'`
+    mgmt_index=`echo ${vlds} | jq 'map(."short-name" == "FwAndServerNs_mgmt_vld") | index(true)'` 
+    prv_index=`echo ${vlds} | jq 'map(."short-name" == "FwAndServerNs_prv_vld_1") | index(true)'`
+    #echo "Mgmt network array index is $mgmt_index"
+    #echo "Prv network array index is $prv_index"
+    NSDetails1="{\"FirewallId\":$fw_id,\"ServerId\":${server_id},\"FirewallMgmtIndex\":\"${mgmt_index}\",\"FirewallPrvIndex\":\"${prv_index}\",\"ServerMgmtIndex\":\"${mgmt_index}\",\"ServerPrvIndex\":\"${prv_index}\"}"
+   #abc='"aa":"${fw_id}"'
+   abc=""
+    echo $NSDetails1
+   # echo "${abc}"
 fi
 return $RETURN_CODE
 }
